003e7b2c237e888a2a29df465780559fe3252d9a
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-07-31 15:11:01 +0200

    add validation to form and submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -8,24 +8,46 @@ renderField(field){
     <div className="form-group">
     <label>{field.label}</label>
     <input className="form-control"type="text" {...field.input}/>
+    {field.meta.touched ? field.meta.error :''}
     </div>
   )
 }
-
+onSubmit(values){
+  console.log(values);
+}
 
 
   render() {
+
+    const {handleSubmit}=this.props;
+
     return (
-    <form>
+    <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
     <Field label="Title" name='title' component={this.renderField}/>
     <Field label="Categories" name='categories' component={this.renderField}/>
     <Field label="Post Content" name='content' component={this.renderField}/>
+    <button type="submit" className="btn btn-primary">Submit</button>
     </form>
 
     )
   }
 }
 
+function validate(values){
+const errors={};
+if(!values.title){
+  errors.title="Enter a title!"
+}
+if(!values.categories){
+  errors.categories="Enter some categories!"
+}
+if(!values.content){
+  errors.content="Enter some content!"
+}
+return errors;
+}
+
 export default reduxForm({
+  validate:validate,
   form:'PostsNewForm'
 })(PostsNew);
